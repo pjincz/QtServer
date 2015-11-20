f9419bb65566f8342aebdf5914af15b9886401bd
jinchizhong <jinchizhong@kingsoft.com>
2015-11-20 19:03:38 +0800

    pettify response headers

--- src/qhttpresponse.cpp
+++ src/qhttpresponse.cpp
@@ -16,13 +16,33 @@ void QHttpResponse::reset()
 	status = 200;
 }
 
+static QString capitalize(const QString & x)
+{
+	QString r = x;
+	bool cap = true;
+	for (int i = 0; i < r.length(); ++i)
+	{
+		if (r[i].isLetterOrNumber())
+		{
+			if (cap)
+				r[i] = r[i].toUpper();
+			cap = false;
+		}
+		else
+		{
+			cap = true;
+		}
+	}
+	return r;
+}
+
 QByteArray QHttpResponse::serialize()
 {
 	headers["Content-Length"] = QString::number(body.length());
 	QByteArray r = QByteArray("HTTP/1.1 ") + QByteArray::number(status) + QByteArray(" OK\r\n");
 	for (QHttpHeaders::iterator it = headers.begin(); it != headers.end(); ++it)
 	{
-		r += it.key().toLatin1() + ": " + it.value() + "\r\n";
+		r += capitalize(it.key()).toLatin1() + ": " + it.value() + "\r\n";
 	}
 	r += "\r\n";
 	return r + body;
