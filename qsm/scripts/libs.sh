8d22d3d14d1a575a9cdfdea8338ef8db1747b61d
jinchizhong <jinchizhong@kingsoft.com>
2015-12-11 22:32:20 +0800

    add qsm install && premake check

--- qsm/scripts/libs.sh
+++ qsm/scripts/libs.sh
@@ -9,13 +9,13 @@ QSM_SCRIPTS="$QSM_ROOT/scripts"
 
 _message()
 {
-	local TEXT=$1
+	local TEXT="$1"
 	shift
-	local COLOR=$1
+	local COLOR="$1"
 	shift
 	for x; do
 		printf "\033[%sm%s%s\033[0m\n" "$COLOR" "$TEXT" "$x"
-		local TEXT=`echo "$TEXT" | sed 's/./ /g'`
+		TEXT=`echo "$TEXT" | sed 's/./ /g'`
 	done 
 }
 
@@ -26,17 +26,17 @@ warning()
 
 info()
 {
-	_message "Warning: " "0;34" "$@"
+	_message "Information: " "0;34" "$@"
 }
 
 error()
 {
-	_message "Warning: " "1;31" "$@"
+	_message "Error: " "1;31" "$@"
 	exit 1
 }
 
 usage()
 {
-	_message "Warning: " "0;34" "$@"
+	_message "Usage: " "0;34" "$@"
 	exit 0
 }
